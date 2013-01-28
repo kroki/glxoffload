e5ed97b034bbf57c826d6b661f6f630ad557ff73
Tomash Brechko <tomash.brechko@gmail.com>
2013-01-28 22:59:13 +0400

    Add library skeleton.

---
+++ src/libGL.c
@@ -0,0 +1,22 @@
+/*
+  Copyright (C) 2013 Tomash Brechko.  All rights reserved.
+
+  This file is part of kroki/glxoffload.
+
+  Kroki/glxoffload is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Kroki/glxoffload is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with kroki/glxoffload.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
